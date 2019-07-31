13a7ceb10a747d554ceb037758b0e85a3182a705
Jim Diaz <james.diaz@ieee.org>
2019-07-31 19:52:57 -0400

    Create and initialize variable a value of 9

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,24 @@
+// 4 Initializing Variables with the Assignment Operator
+/*
+
+* initialize a variable to an initial value
+ in the same line as it is declared.
+
+var myVar = 0;
+
+Creates a new variable called myVar and assigns it an initial value of 0.
+
+--- Assignment ---
+Define a variable a with var and initialize it to a value of 9.
+
+-- Commit --
+Create and initialize variable a value of 9
+
+-----*/
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+
+var a = 9;
