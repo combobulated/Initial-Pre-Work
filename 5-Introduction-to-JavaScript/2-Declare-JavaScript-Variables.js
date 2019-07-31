d0000ce99fe28a71ed0876b51ea10d34ec9708ee
Jim Diaz <james.diaz@ieee.org>
2019-07-31 19:41:28 -0400

    Add variable myName for variable demo
    Note variable type is implied

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,31 @@
+// 2 Declare JavaScript Variables
+
+
+variable data types are:
+* undefined, null, boolean, string, symbol, number, and object.
+
+example
+var ourName;
+
+* keyword var creates a variable called ourName.
+* end statements with semicolons. ";"
+
+* Variable names can be made up of numbers, letters, and $ or _,
+* but may not contain spaces or start with a number.
+
+* use camelcase... small letter for first syllable, rest are caps
+  ie myVarIsEasierReadingInCamelCase
+
+---- Assignment ---
+Use the var keyword to create a variable called myName.
+
+
+--- Commits ---
+Add variable myName for variable demo
+
+----- */
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
