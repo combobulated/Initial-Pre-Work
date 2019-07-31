4178473b762211adff0f8f06355f7d9d8f0cab1c
Jim Diaz <james.diaz@ieee.org>
2019-07-31 19:24:22 -0400

    Add in-line and multiline comments
    // is a single line comment
    /*  and  */  incloses multiline comments

--- 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
+++ 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
@@ -1,3 +1,32 @@
-// Please remember to include a summary statement with EACH FILE regarding what you learned from the lesson in this format!  I recommend keeping it at the top of each file for faster processing of your application.
 
-// Copy your code from the central panel of the freeCodeCamp page and paste it below after successfully passing the test.  You will have to click on the X at the top right of the modal that pops up to see the code. If you forget, just rerun the test.  You are also welcome to download your solution.
+// 1 Comment Your JavaScript Code
+
+//Comments are lines of code that JavaScript will intentionally ignore.
+
+
+//
+// Inline C"omments
+//Using "//" will tell JavaScript to ignore the remainder of the
+
+// ie
+// This is an in-line comment.
+
+// Multiline comments
+// ie
+/* This is a
+multi-line comment */
+
+/*
+--- Assignment ---
+Try creating comments; one of each type of comment.
+
+---- Commits --
+
+Add comments to code with /* and //
+
+*/
+
+// example of an in-line comment
+
+/* a multi
+line comment */
