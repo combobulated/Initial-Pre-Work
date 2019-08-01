586996b2a7b0d12be3c37a50bf8044335879bcfe
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:35:44 -0400

    Change the code to use the ++ operator on myVar.
    this commit changes myVar = myVar + 1 to short representation
    
    myVar++;

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,33 @@
+// 11 Increment a Number with JavaScript
+
+/*
+ increment or add one to a variable with the ++ operator.
+
+i++;
+
+is the equivalent of
+
+i = i + 1;
+
+Note
+The entire line becomes i++;, eliminating the need for the equal sign.
+
+--- Assignment --
+Change the code to use the ++ operator on myVar.
+
+Hint
+Learn more about Arithmetic operators - Increment (++).
+
+--- Commits ----
+Change the code to use the ++ operator on myVar.
+
+this commit changes myVar = myVar + 1 to short representation
+
+myVar++;
+
+---------------*/
+
+var myVar = 87;
+
+// Only change code below this line
+ myVar++;
