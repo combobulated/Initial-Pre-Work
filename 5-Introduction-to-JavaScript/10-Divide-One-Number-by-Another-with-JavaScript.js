f1a6fb8628bd774a01a0d3d32bdc863efd38a458
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:30:32 -0400

    Dvide so result is 22
    Change the 0 so that the var "quotient" is equal to 2.

---
+++ 5-Introduction-to-JavaScript/10-Divide-One-Number-by-Another-with-JavaScript.js
@@ -0,0 +1,19 @@
+// 10 Divide One Number by Another with JavaScript
+/*
+* divide one number by another.
+
+JavaScript uses the / symbol for division.
+
+Example
+
+myVar = 16 / 2; // assigned 8
+--- Assignment --
+Change the 0 so that the quotient is equal to 2.
+
+---- Commits ---
+Dvide so result is 22
+
+Change the 0 so that the var "quotient" is equal to 2.
+
+--*/
+var quotient = 66 /33;
