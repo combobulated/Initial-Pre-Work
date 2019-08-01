1e94d9318bc6025c2baa576720e44fe60792a8d3
Jim Diaz <james.diaz@ieee.org>
2019-07-31 21:00:03 -0400

    Division
    Change the 0.0 so that quotient will equal to 2.2.

---
+++ 5-Introduction-to-JavaScript/15-Divide-One-Decimal-by-Another-with-JavaScript.js
@@ -0,0 +1,15 @@
+// 15 Divide One Decimal by Another with JavaScript
+
+/* divide one decimal by another.
+
+--- Assignment ---
+Change the 0.0 so that quotient will equal to 2.2.
+
+---- Commits ---
+Division
+
+Change the 0.0 so that quotient will equal to 2.2.
+
+----*/
+
+var quotient = 4.4 / 2.0; 
