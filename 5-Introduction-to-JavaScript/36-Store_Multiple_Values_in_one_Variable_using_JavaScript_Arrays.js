a7466b13344e5e140a0154d2c433a0db9c1a2529
Jim Diaz <james.diaz@ieee.org>
2019-08-01 17:49:04 -0400

    Make a simple array with an number and String
    Creates array myArray = "lap top", 6];

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,32 @@
+// 36 Store Multiple Values in one Variable using JavaScript Arrays
+// source: learn.freecodecamp.org
+
+/*
+"array variables"
+With JavaScript array variables,
+we can store several pieces of data in one place.
+( this example shows various data types can be stored in same array )
+
+You start an array declaration with an opening square bracket,
+end it with a closing square bracket, and put a comma between each entry,
+like this:
+
+var sandwich = ["peanut butter", "jelly", "bread"].
+
+------ Assignments ----
+Modify the new array myArray so that it contains both a string
+ and a number (in that order).
+
+Hint
+Refer to the example code in the text editor if you get stuck.
+
+-----Commits---
+Make a simple array with an number and String
+
+Creats array myArray = "ap top", 6];
+----------*/
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["lap top", 6];
