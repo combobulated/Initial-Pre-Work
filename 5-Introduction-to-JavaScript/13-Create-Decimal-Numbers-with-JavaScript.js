a2a14f1242f2ac5fea9de7285afc1b26827ab819
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:47:04 -0400

    Assign decimal 10.2 to var myDecimal

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,25 @@
+// 13  Create Decimal Numbers with JavaScript
+
+/*
+ store decimal numbers in variables.
+Decimal numbers referred to as floating point numbers or floats.
+
+Note
+Not all real numbers can accurately be represented in floating point.
+This can lead to rounding errors.
+
+----- Assignment ----
+Create a variable myDecimal and give it a decimal value
+with a fractional part (e.g. 5.7).
+
+---- Commits ---
+Assign decimal 10.2 to var myDecimal
+
+
+
+----*/
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 10.2;
