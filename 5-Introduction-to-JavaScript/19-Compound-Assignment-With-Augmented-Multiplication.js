c211f4d76c1cbcad1f0a19d42fe1b7fe052a2a52
Jim Diaz <james.diaz@ieee.org>
2019-07-31 21:38:12 -0400

    Convert assignments to Compound Assignment ( *= )
    Uses the *= operator to change a = a * num to
    the style of  a *= num; for vars a, b and c
    This is called Augmented Multiplication

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,34 @@
+// 19 Compound Assignment With Augmented Multiplication
+
+/*
+The *= operator multiplies a variable by a number.
+
+myVar = myVar * 5;
+
+will multiply myVar by 5. This can be rewritten as:
+
+myVar *= 5;
+
+--- Assignment ---
+
+Convert the assignments for a, b, and c to use the *= operator.
+
+--- Commits --
+Convert assignments to Compound Assignment ( *= )
+
+Uses the *= operator to change a = a * num to
+the style of  a *= num; for vars a, b and c
+This is called Augmented Multiplication
+
+
+----*/
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *=5 ;
+b *= 3;
+c *= 10;
