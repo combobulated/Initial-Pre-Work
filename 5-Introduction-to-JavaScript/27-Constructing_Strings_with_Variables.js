94e749a0e6bb021e6e833ba01776b3166acfee78
Jim Diaz <james.diaz@ieee.org>
2019-08-01 00:22:51 -0400

    Construct a string with inclusion of a variable
    Use concatenation to string var myName inbetween two
    string literals; "My Name is" and " And I am well"

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,25 @@
+// 27  Constructing Strings with Variables
+
+/*
+Sometimes you will need to build a string, Mad Libs style.
+By using the concatenation operator (+), you can insert one or more
+variables into a string you're building.
+
+----- Assignment ----
+Set myName to a string equal to your name and build myStr with
+myName between the strings "My name is " and " and I am well!"
+
+----- Commits ---
+Construct a string with inclusion of a variable
+
+Use concatenation to string var myName inbetween two
+string literals; "My Name is" and " And I am well"
+
+----------------*/
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Jim";
+var myStr = "My Name is " + myName + " and I am well!" ;
