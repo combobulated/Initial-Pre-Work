686a2a8b7ed4e1eaa495cac913831a7134bab9d3
Jim Diaz <james.diaz@ieee.org>
2019-08-01 00:04:43 -0400

    Concatenate two strings with Plus operator
     Builds myStr from the strings "This is the start. " and "This is the end."
      using the + operator.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,33 @@
+// 25 Concatenating Strings with Plus Operator
+/*
+In JavaScript, when the + operator is used with a String value,
+it is called the concatenation operator.
+
+Build new strings out of other strings by concatenating them together.
+
+Example
+
+'My name is Alan,' + ' I concatenate.'
+
+Note
+Watch out for spaces. Concatenation does not add spaces between
+concatenated strings, so you'll need to add them yourself.
+
+---- Assignment --
+Build myStr from the strings "This is the start. " and "This is the end."
+ using the + operator.
+
+ ---- Commits --
+ Concatenate two strings with Plus operator
+
+ Builds myStr from the strings "This is the start. " and "This is the end."
+  using the + operator.
+
+  ------*/
+
+  // Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. "+"This is the end.";
