eabfc41d1a6946996b0b4eff12939890d069f134
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:18:27 -0400

    Change the 0 so that sum will equal 20.

---
+++ 5-Introduction-to-JavaScript/7-Add-Two-Numbers-with-JavaScript.js
@@ -0,0 +1,26 @@
+// 7 Add Two Numbers with JavaScript
+
+/*
+
+Number is a data type in JavaScript
+* it represents numeric data.
+
+
+
+JavaScript uses the + symbol as addition operation
+when placed between two numbers.
+
+Example
+
+myVar = 5 + 10; // assigned 15
+
+--- Assignment ---
+
+Change the 0 so that sum will equal 20.
+
+--- Commits ---
+
+Change the 0 so that sum will equal 20.
+
+---- */
+var sum = 10 + 10;
