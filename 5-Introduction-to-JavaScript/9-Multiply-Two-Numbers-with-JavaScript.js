e2597e87163f36a1b058dbe312a5238f7764eb53
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:25:48 -0400

    Change the 0 so that product will equal 80.

---
+++ 5-Introduction-to-JavaScript/9-Multiply-Two-Numbers-with-JavaScript.js
@@ -0,0 +1,20 @@
+// 9 Multiply Two Numbers with JavaScript
+/*
+* multiply one number by another.
+
+JavaScript uses the * symbol for multiplication of two numbers.
+
+Example
+
+myVar = 13 * 13; // assigned 169
+
+--- assignment ---
+
+Change the 0 so that product will equal 80.
+
+---- commits ---
+Change the 0 so that product will equal 80.
+
+----*/
+
+var product = 8 * 10;
