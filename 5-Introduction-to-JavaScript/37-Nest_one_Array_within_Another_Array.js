b6ee972e968d5a355686c37d525a4acdc59baa3a
Jim Diaz <james.diaz@ieee.org>
2019-08-01 17:57:40 -0400

    Create a multidimensional arrays
    this commit creates an array with two inner arrays
    ie var myArray = [ [1,2,],["shoe","glove"]];

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,26 @@
+// 37 Nest one Array within Another Array
+// source: learn.freecodecamp.org
+
+/* "nesting arrays" (multi-dimensional arrays) LOL! !FORTRAN
+
+* nest arrays within other arrays,
+ like this: [["Bulls", 23], ["White Sox", 45]].
+
+ This is also called a Multi-dimensional Array.
+
+---- Assignment---
+Create a nested array called myArray.
+
+--- Commits---
+Create a multidimensional arrays
+
+this commit creates an array with two inner arrays
+ie var myArray = [ [1,2,],["shoe","glove"]];
+
+------------*/
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [ [1,2,],["shoe","glove"]];
