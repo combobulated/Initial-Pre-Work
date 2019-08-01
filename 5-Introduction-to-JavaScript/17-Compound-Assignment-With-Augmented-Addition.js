0ed089f077b0170607796030181c9fb1cb8ff2d9
Jim Diaz <james.diaz@ieee.org>
2019-07-31 21:29:00 -0400

    Convert assignments to Compound Assignment ( += )
    Uses the += operator to change a = a + num to
    the style of  a += num; for vars a, b and c

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,43 @@
+// 17 Compound Assignment With Augmented Addition
+/*
+* use assignments to modify
+the contents of a variable.
+
+* Remember that everything to the right of the equals sign is evaluated first,
+so we can say:
+
+myVar = myVar + 5;
+to add 5 to myVar.
+
+* Since this is such a common pattern,
+there are operators which do both a mathematical operation
+and assignment in one step.
+
+* the += operator.
+
+var myVar = 1;
+myVar += 5;
+console.log(myVar); // Returns 6
+
+---- Assignment ----
+Convert the assignments for a, b, and c to use the += operator.
+
+--- Commits ----
+
+Convert assignments to Compound Assignment ( += )
+
+Uses the += operator to change a = a + num to
+the style of  a += num; for vars a, b and c
+
+--- */
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+
+// Only modify code below this line
+
+ a += 12;
+ b += 9;
+ c += 7;
