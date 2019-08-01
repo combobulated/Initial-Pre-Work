6cddd2bc12ce4ee34a5a975160d376224feb7054
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:22:10 -0400

    Change the 0 so the difference is 12.

---
+++ 5-Introduction-to-JavaScript/8-Subtract-One-Number-from-Another-with-JavaScript.js
@@ -0,0 +1,19 @@
+// 8 Subtract One Number from Another with JavaScript
+
+/*
+subtract one number from another.
+
+JavaScript uses the - symbol for subtraction.
+
+Example
+
+myVar = 12 - 6; // assigned 6
+
+---assignment--
+Change the 0 so the difference is 12.
+
+--- commits --
+Change the 0 so the difference is 12.
+-----*/
+
+var difference = 45 - 33;
