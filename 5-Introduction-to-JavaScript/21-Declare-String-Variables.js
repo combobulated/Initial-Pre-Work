c02ad4e8aae0b5be0824fbfa11820ae414e7afdf
Jim Diaz <james.diaz@ieee.org>
2019-07-31 21:51:00 -0400

    Add two new vars and assign them string literals

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,29 @@
+// 21 Declare String Variables
+/*
+"string literals"
+
+var myName = "your name";
+
+"your name" is called a string literal.
+
+* It is a string because it is a series of zero or more characters
+enclosed in single or double quotes.
+
+--- Assignments ---
+
+Create two new string variables: myFirstName and myLastName
+and assign them the values of your first and last name, respectively.
+
+----- Commits --
+Add two new vars and assign them string literals
+
+---------------*/
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Jim"
+var myLastName = "Diaz";
