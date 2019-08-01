e33c7d451787eb356274795ca5699dbe3d86ae68
Jim Diaz <james.diaz@ieee.org>
2019-08-01 19:10:54 -0400

    Create a shopping list of 5 items
    Creates myList array of 5 items.  Each item is a 2 element subarray.
    Each subarray has a string and a number.  Each string is a grocery item.
    Each number is qty of grocery item.

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,30 @@
+// 45 Shopping List
+// source: learn.freecodecamp.org
+
+/* "create a shopping list of 5 items and qty of each item"
+
+--- Assignment ---
+Create a shopping list in the variable myList. The list should be a
+ multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string
+with the name of the item.
+
+The second element should be a number representing the quantity
+
+i.e.
+
+["Chocolate Bar", 15]
+
+There should be at least 5 sub-arrays in the list.
+
+--- commits ---
+Create a shopping list of 5 items
+
+Creates myList array of 5 items.  Each item is a 2 element subarray.
+Each subarray has a string and a number.  Each string is a grocery item.
+Each number is qty of grocery item.
+
+ --------------*/
+ var myList = [];
+var myList = [[ "hot dogs", 5], ["turkey", 1], ["bananas", 5], ["grapes", 1], ["garlic", 3]];
