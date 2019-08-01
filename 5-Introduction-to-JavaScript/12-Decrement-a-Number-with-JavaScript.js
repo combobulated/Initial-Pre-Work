f08ca2383208a6c3f463abafffec495cd40c6e0c
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:39:33 -0400

    Change the code to use the -- operator on myVar.
    Changes line to i--;, eliminating the need for the equal sign.

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,29 @@
+// 12 Decrement a Number with JavaScript
+
+/*
+You can easily decrement or decrease a variable by one with the -- operator.
+
+i--;
+
+is the equivalent of
+
+i = i - 1;
+
+Note
+The entire line becomes i--;, eliminating the need for the equal sign.
+
+
+--- Assignment ---
+
+Change the code to use the -- operator on myVar.
+
+---- Commits ---
+Change the code to use the -- operator on myVar.
+
+Changes line to i--;, eliminating the need for the equal sign.
+
+----*/
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
