8f817fdd4c4150eebfa8be18eca11958ea2c6d2d
Jim Diaz <james.diaz@ieee.org>
2019-07-31 20:51:38 -0400

    Change the 0.0 so that product will equal 5.0.

---
+++ 5-Introduction-to-JavaScript/14-Multiply-Two-Decimals-with-JavaScript.js
@@ -0,0 +1,15 @@
+//14 Multiply Two Decimals with JavaScript
+
+/*
+
+
+ multiply two decimals together to get their product.
+
+---- Assignment ----
+Change the 0.0 so that product will equal 5.0.
+
+--- Commits ----
+Change the 0.0 so that product will equal 5.0.
+
+---- */
+var product = 2.0 * 2.5;
