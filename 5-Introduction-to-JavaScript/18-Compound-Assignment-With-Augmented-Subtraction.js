48d30170ed5e06de065daba794d42783a1b429be
Jim Diaz <james.diaz@ieee.org>
2019-07-31 21:33:11 -0400

    Convert assignments to Compound Assignment ( -= )
    Uses the -= operator to change a = a - num to
    the style of  a -= num; for vars a, b and c
    This is called Augmented Subtraction

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,32 @@
+// 18 Compound Assignment With Augmented Subtraction
+
+/*
+
+Like the += operator, -= subtracts a number from a variable.
+
+myVar = myVar - 5;
+
+will subtract 5 from myVar. This can be rewritten as:
+
+myVar -= 5;
+
+--- Assignment ---
+Convert the assignments for a, b, and c to use the -= operator.
+
+---- Commits ---
+Convert assignments to Compound Assignment ( -= )
+
+Uses the -= operator to change a = a - num to
+the style of  a -= num; for vars a, b and c
+This is called Augmented Subtraction
+--- */
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
