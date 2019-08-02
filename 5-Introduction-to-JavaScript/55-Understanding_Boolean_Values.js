1d5b014075c01c8e756ac91223047fbe423e1033
Jim Diaz <james.diaz@ieee.org>
2019-08-01 21:20:52 -0400

    Return boolean true rather than false
    Modifies welcomToBooleans function to return true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,32 @@
+// 55 Understanding Boolean Values
+// Source: learn.freecodecamp.org
+
+/* "Booleans true and false"
+
+Another data type is the Boolean.
+Booleans may only be one of two values: true or false.
+They are basically little on-off switches, where true
+is "on" and false is "off." These two states are mutually exclusive.
+
+Note
+Boolean values are never written with quotes. The strings "true" and "false"
+ are not Boolean and have no special meaning in JavaScript.
+
+------- Assignment ---
+Modify the welcomeToBooleans function so that it returns
+true instead of false when the run button is clicked.
+
+------ Commits -----
+Return boolean true rather than false
+
+Modifies welcomToBooleans function to return true
+
+---------------*/
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
