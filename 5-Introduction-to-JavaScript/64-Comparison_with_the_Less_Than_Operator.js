6e073fc5050ce118dd1ced533345529a94a88da8
Jim Diaz <james.diaz@ieee.org>
2019-08-01 23:07:03 -0400

    Add less than operator <
    Adds the less than operator to the indicated lines so that the
    return statements make sense.

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,26 @@
+// 64  Comparison with the Less Than Operator
+// source: learn.freecodecammp.org
+
+The less than operator (<) compares the values of two numbers.
+If the number to the left is less than the number to the right,
+it returns true. Otherwise, it returns false. Like the equality
+operator, less than operator converts data types while comparing.
+
+Examples
+
+2 < 5 // true
+'3' < 7 // true
+5 < 5 // false
+3 < 2 // false
+'8' < 4 // false
+
+---- Assignments ---
+Add the less than operator to the indicated lines so that the
+return statements make sense.
+
+---- Commits -----
+Add less than operator <
+
+Adds the less than operator to the indicated lines so that the
+return statements make sense.
+--------------*
