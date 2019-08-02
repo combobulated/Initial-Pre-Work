ffce50c9ab52de39d4d3e202d5e2078ffa236187
Jim Diaz <james.diaz@ieee.org>
2019-08-02 18:05:25 -0400

    Use for loop to iterate odd Numbers
    Pushes the odd numbers from 1 through 9 to myArray using a for loop.

--- 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -24,4 +24,19 @@ Push the odd numbers from 1 through 9 to myArray using a for loop.
 Use for loop to iterate odd Numbers
 
 Pushes the odd numbers from 1 through 9 to myArray using a for loop.
-----------------*
+----------------*/
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for( var i = 1; i< 10; i +=2 ){
+
+  myArray.push(i);
+}
